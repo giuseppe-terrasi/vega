9369582743ee0961f59ddcf0dcf19c17372861bd
Giuseppe Terrasi <giuseppe.terrasi@hotmail.it>
2018-06-07 10:01:09 +0200

    Aggiunto api per le features

---
+++ Vega/Controllers/FeaturesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vega.Controllers.Resources;
+using Vega.Models;
+using Vega.Persistence;
+
+namespace Vega.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly VegaDbContext context;
+        private readonly IMapper mapper;
+
+        public FeaturesController(VegaDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet("")]
+        public async Task<IEnumerable<FeatureResource>> GetFeatures()
+        {
+            var features = await context.Features.ToListAsync();
+
+            return mapper.Map<List<Feature>, List<FeatureResource>>(features);
+        }
+
+
+    }
+}
