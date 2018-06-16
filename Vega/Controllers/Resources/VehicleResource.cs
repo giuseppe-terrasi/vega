8c73bfb4730420d4a1faed9cbfd3ef1ea773531e
Giuseppe Terrasi <giuseppe.terrasi@hotmail.it>
2018-06-16 18:55:16 +0200

    Completato Vehicle API

---
+++ Vega/Controllers/Resources/VehicleResource.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Vega.Controllers.Resources
+{
+
+    public class VehicleResource
+    {
+        public int Id { get; set; }
+
+        public int ModelId { get; set; }
+
+        public bool IsRegistered { get; set; }
+
+        [Required]
+        public ContactResource Contact { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+
+        public ICollection<int> Features { get; set; }
+
+        public VehicleResource()
+        {
+            Features = new Collection<int>();
+        }
+    }
+}
---
+++ Vega/Controllers/VehiclesController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vega.Controllers.Resources;
+using Vega.Models;
+using Vega.Persistence;
+
+namespace Vega.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly VegaDbContext context;
+
+        public VehiclesController(IMapper mapper, VegaDbContext context)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            vehicle.LastUpdate = DateTime.Now;
+
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+                return NotFound();
+
+            mapper.Map(vehicleResource, vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+                return NotFound();
+
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+                return NotFound();
+
+            var vehicleResource = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(vehicleResource);
+        }
+    }
+}
